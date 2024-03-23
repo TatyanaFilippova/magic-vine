ef578da8d2736bd9ef71a2895f543e2a3905d9d2
Tatiana <tatiana.filippova.98@mail.ru>
2024-03-23 13:29:54 +0300

    Собрала главную.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import css from "./page.module.css";
+import Header from "@/components/Header/Header";
+import BannerMiddle from "@/components/BannerMiddle/BannerMiddle";
+import ProductCard from "@/components/ProductCard/ProductCard";
+import ReviewCard from "@/components/ReviewCard/ReviewCard";
 
 export default function Home() {
-  return <div>dghj</div>;
+    return <div>
+        <Header></Header>
+        <div className={css.productCardWrapper}>
+            <ProductCard title={"Товар 1"} text={"Описание товара"}></ProductCard>
+            <ProductCard title={"Товар 1"} text={"Описание товара"}></ProductCard>
+            <ProductCard title={"Товар 1"} text={"Описание товара"}></ProductCard>
+            <ProductCard title={"Товар 1"} text={"Описание товара"}></ProductCard>
+            <ProductCard title={"Товар 1"} text={"Описание товара"}></ProductCard>
+            <ProductCard title={"Товар 1"} text={"Описание товара"}></ProductCard>
+        </div>
+        <BannerMiddle description={"Описанние баннера"} text1={"Длинное описание баннера, акции"}
+                      text2={"Может быть в несколько строк"}></BannerMiddle>
+        <div className={css.text}> Отзывы
+           <div className={css.reviewCardWrapper}><ReviewCard text1={"Ирина Васильевна"} text2={"Очень классная корзинка, прям вообще огонь"}></ReviewCard>
+               <ReviewCard text1={"Ирина Васильевна"} text2={"Очень классная корзинка, прям вообще огонь"}></ReviewCard>
+               <ReviewCard text1={"Ирина Васильевна"} text2={"Очень классная корзинка, прям вообще огонь"}></ReviewCard>
+           </div>
+
+        </div>
+    </div>;
+
 }
